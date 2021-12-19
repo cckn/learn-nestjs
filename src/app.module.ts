15ea8d54a53222b3054596593a6bfd8e0ec77fb8
cckn <cckn.dev@gmail.com>
2021-12-20 01:18:56 +0900

    ## NestJS와 DB 연결하기, 환경 변수 설정
    https://www.inflearn.com/course/%ED%83%84%ED%83%84%ED%95%9C-%EB%B0%B1%EC%97%94%EB%93%9C-%EB%84%A4%EC%8A%A4%ED%8A%B8/lecture/83828?tab=note&speed=1.25
    
    ### 몽고 DB 사용 https://docs.nestjs.kr/techniques/mongodb
    
    - [x] 몽구스 설치
    ```bash
    npm install --save @nestjs/mongoose mongoose
    ```
    - [x] 몽고 DB URL 획득 - application
    	![[Pasted image 20211220011128.png]]
    
    ### 환경변수 설정 https://docs.nestjs.kr/techniques/configuration
    
    사용하려면 모듈에서 import를 해주어야 함
    몽고DB URI 설정
    port도 env로
    gitignore에 .env 추가
    
    ### 몽고DB 옵션 지정
    ![[Pasted image 20211220010821.png]]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { LoggerMiddleware } from './common/middlewares/logger.middleware';
-
 import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }),
+    CatsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
